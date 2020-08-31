8aff1b3a4e9b4f6c163d56336c0584f2ca179c3a
andiaditya21 <andiaditya21@gmail.com>
2020-08-31 21:50:49 +0800

    pending dulu

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World </h1>
+      <h1>PENDING DULU</h1>
+      <p>Dilanjutkan setelah menyelesaikan BWA - Fullstack javascript</p>
     </div>
   );
 }
