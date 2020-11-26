63bd9f3efda60eabb25743a680f966fad525dd59
andiaditya21 <andiaditya21@gmail.com>
2020-11-26 20:17:59 +0800

    rebuild website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>PENDING DULU</h1>
-      <p>Dilanjutkan setelah menyelesaikan BWA - Fullstack javascript</p>
-    </div>
-  );
+  return <h1>Rebuild website</h1>;
 }
 
 export default App;
